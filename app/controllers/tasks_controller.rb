3d269e350636bb06664ab7c00e504c0509b947e0
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-17 09:15:16 +0900

    add update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,5 +5,26 @@ class TasksController < ApplicationController
     render json: tasks
   end
 
+  def create
+    Task.create(task_params)
+    head :created
+  end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    head :ok
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    head :ok
+  end
+
+
+  private 
+  def task_params
+    params.permit(:name, :is_done)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "tasks" => "tasks#index"
+  post "tasks" => "tasks#create"
+  delete "tasks/:id" => "tasks#destroy"
+  put "tasks/:id" => "tasks#update"
 end
